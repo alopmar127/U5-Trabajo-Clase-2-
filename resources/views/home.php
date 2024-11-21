15075ef8ae4c2ce7bf8656b38b785e28f7d93b96
alopmar127 <alopmar127g@g.educaand.es>
2024-11-21 20:53:33 +0100

    Nada, es una prueba

--- resources/views/home.php
+++ resources/views/home.php
@@ -11,27 +11,28 @@
 <body>
     <p class="header-paragraph">Pruebas de consultas (hacer scroll):</p>
     <?php
-     use App\Models\UsuarioModel; // Recuerda el uso del autoload.php
-     
-     // Se instancia el modelo
-     $usuarioModel = new UsuarioModel();
 
-     // Descomentar consultas para ver la creación. Cuando se lanza execute hay código para
-     // mostrar la consulta SQL que se está ejecutando.
-     
-     // Consulta 
+    use App\Models\UsuarioModel; // Recuerda el uso del autoload.php
+
+    // Se instancia el modelo
+    $usuarioModel = new UsuarioModel();
+
+    // Descomentar consultas para ver la creación. Cuando se lanza execute hay código para
+    // mostrar la consulta SQL que se está ejecutando.
+
+    // Consulta 
     //  $usuarioModel->all();
 
-     // Consulta
-     //$usuarioModel->select('columna1', 'columna2')->get();
+    // Consulta
+    //$usuarioModel->select('columna1', 'columna2')->get();
 
-     // Consulta
+    // Consulta
     //  $usuarioModel->select('columna1', 'columna2')
     //              ->where('columna1', '>', '3')
     //              ->orderBy('columna1', 'DESC')
     //              ->get();
 
-     // Consulta
+    // Consulta
     //  $usuarioModel->select('columna1', 'columna2')
     //              ->where('columna1', '>', '3')
     //              ->where('columna2', 'columna3')
@@ -40,16 +41,16 @@
     //              ->orderBy('columna1', 'DESC')
     //              ->get();
 
-     // Consulta
+    // Consulta
     //  $usuarioModel->create(['id' => 1, 'nombre' => 'nombre1', 'apellidos' => 'apellidos1']);
 
-     // Consulta
-     //$usuarioModel->delete(['id' => 1]);
+    // Consulta
+    //$usuarioModel->delete(['id' => 1]);
 
-     // Consulta
+    // Consulta
     //  $usuarioModel->update(['id' => 1], ['nombre' => 'NombreCambiado']);
 
-     echo "Pruebas SQL Query Builder";
+    echo "Pruebas SQL Query Builder";
     ?>
 </body>
 
