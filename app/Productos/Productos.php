af09b4d6be86ae45937f7cca99a8fac2aee1f5d6
alopmar127 <alopmar127g@g.educaand.es>
2024-11-24 09:27:29 +0100

    Interfaz VendibleInterface
    Interfaz pedida en el ejercicio que obliga a todas las clases que lo implementen haciendo que todas las clases hijas de productos tengan que tener un IVA

---
+++ app/Productos/Productos.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Productos;
+
+use App\Interfaces\VendibleInterface;
+
+//Clase abstracta debido a que implementa un metodo abstracto y además se definira mejor en sus clases hijas
+abstract class Productos implements VendibleInterface
+{
+
+    // En esta carpeta se podrían crear más clases para otros tipos de productos
+    // Se pueden crear más carpetas en app/ para organizar las clases
+
+    //Constante con el IVA
+    public const IVA = 21;
+
+    //Sintaxis Constructor Property Promotion
+    public function __construct(private string $id, private string $nombre, private float $precio) {}
+    //Metodo abstracto que definiremos en las clases hijas
+    abstract public function mostrarDescripcion(): void;
+
+    //Funcion que calcula el precio con IVA
+    public function calcularPrecioConIva(): float
+    {
+        return $this->precio * (1 + self::IVA / 100);
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function setId(string $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getNombre(): string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): void
+    {
+        $this->nombre = $nombre;
+    }
+
+    public function getPrecio(): float
+    {
+        return $this->precio;
+    }
+
+    public function setPrecio(float $precio): void
+    {
+        $this->precio = $precio;
+    }
+}
---
+++ app/Productos/VendibleInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Interfaces;
+
+interface VendibleInterface
+{
+    public function calcularPrecioConIVA(): float;
+}
