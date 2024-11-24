afa62a29a4ea01c91eed724ace6030af8f43a1b9
alopmar127 <alopmar127g@g.educaand.es>
2024-11-24 09:28:13 +0100

    Añadido comentario a VendibleInterface

--- app/Productos/VendibleInterface.php
+++ app/Productos/VendibleInterface.php
@@ -2,6 +2,7 @@
 
 namespace App\Interfaces;
 
+//obliga a todas las clases que lo implementen haciendo que todas las clases hijas de productos tengan que tener un IVA
 interface VendibleInterface
 {
     public function calcularPrecioConIVA(): float;
