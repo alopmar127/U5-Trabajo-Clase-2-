e603aa8bff4c969de6fc33263a59bcf6d8529457
alopmar127 <alopmar127g@g.educaand.es>
2024-11-27 16:55:09 +0100

    RopaModel modificado
    Modificado con un metodo para obtener datos de los productos usando el joinTablas de Model y pasandole los campos

---
+++ app/Models/RopaModel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+class RopaModel extends Model
+{
+    protected $table = 'ropa'; // Tabla secundaria
+    protected $table2 = 'productos'; // Tabla principal
+
+    public function obtenerDatosConProducto(): array
+    {
+        return $this->joinTablas(['id', 'talla'], ['id', 'nombre', 'precio']);
+    }
+}
