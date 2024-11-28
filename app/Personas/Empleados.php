b711e85f13ceea722bb048e05fe2c87e1aafabf4
ALM93 <alopmar127g@g.educaand.es>
2024-11-28 20:37:24 +0100

    Cambios constructor empleados
    Quitado que se inserte en la bbdd

--- app/Personas/Empleados.php
+++ app/Personas/Empleados.php
@@ -15,7 +15,26 @@ class Empleados {
     public function __construct(private string $nombre, private string $apellido) {
         $usuarioModel = new UsuarioModel();
         //Al llamar al constructor lo insertamos en la BBDD
-        $usuarioModel->create(['nombre' => $nombre, 'apellidos' => $apellido]);
+        // $usuarioModel->create(['nombre' => $nombre, 'apellidos' => $apellido]);
     }
-    
+
+    //get y set para los atributos
+    public function getNombre(): string
+    {
+        return $this->nombre;
     }
+
+    public function setNombre(string $nombre): void
+    {
+        $this->nombre = $nombre;  
+    }
+    public function getApellido(): string
+    {
+        return $this->apellido;
+    }
+
+    public function setApellido(string $apellido): void
+    {
+        $this->apellido = $apellido;
+    }
+}
