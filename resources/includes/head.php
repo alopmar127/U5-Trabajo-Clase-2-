1d0e790c6825327a47a6b9e35403a2f41bb0c16f
alopmar127 <alopmar127g@g.educaand.es>
2024-11-28 19:07:57 +0100

    Cambio titulo pagina

--- resources/includes/head.php
+++ resources/includes/head.php
@@ -6,5 +6,5 @@ declare(strict_types=1);
 
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Login Cookies - <?php echo isset($tituloPagina) ? $tituloPagina : 'Inicio'; ?></title>
+<title>Tienda - <?php echo isset($tituloPagina) ? $tituloPagina : 'Inicio'; ?></title>
 <link rel="stylesheet" href="/css/styles.css">
