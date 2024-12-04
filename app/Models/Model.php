600d39cf4d42df8a3b98cd02f11555c7352f4476
ALM93 <alopmar127g@g.educaand.es>
2024-12-04 21:46:09 +0100

    Comentarios Model
    Mejorar entendimiento

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -176,31 +176,45 @@ class Model
     // Se añade where a la sentencia con operador específico
     public function where($column, $operator, $value = null, $chainType = 'AND')
     {
+        // Si no se pasa el operador tipo %, =, >, <, IN, NOT IN, BETWEEN, se asume =
         if ($value === null) {
             $value = $operator;
+            // Se asume que el operador es =
             $operator = '=';
         }
-    
+        // Se convierte el operador a mayúsculas
         $operator = strtoupper($operator);
-    
+
+        // Se verifica si el operador es IN o NOT IN y se aplica el formato adecuado
         if (in_array($operator, ['IN', 'NOT IN']) && is_array($value)) {
+            // Se crean las variables para los valores de la consulta 
             $placeholders = implode(', ', array_fill(0, count($value), '?'));
+            // Se crea la condición con el operador y los valores
             $condition = "{$column} {$operator} ({$placeholders})";
+            // Se agregan los valores a las variables
             $this->values = array_merge($this->values, $value);
+        // Se verifica si el operador es BETWEEN y se aplica el formato adecuado
         } elseif ($operator == 'BETWEEN' && is_array($value) && count($value) == 2) {
+            // Se crea la condición con el operador y los valores
             $condition = "{$column} BETWEEN ? AND ?";
+            // Se agregan los valores a las variables
             $this->values = array_merge($this->values, $value);
+        // Si es LIKE se aplica el formato adecuado
         } else {
+            // Se crea la condición con el operador y el valor
             $condition = "{$column} {$operator} ?";
+            // Se agregan los valores a las variables
             $this->values[] = $value;
         }
-    
+        // Se verifica si hay una condición existente y se agrega el operador y la condición
         if ($this->where) {
+            // Se agrega el operador y la condición
             $this->where .= " {$chainType} {$condition}";
+        // En caso contrario, se crea la condición
         } else {
             $this->where = $condition;
         }
-    
+        // Se devuelve para permitir el uso de métodos en cascada
         return $this;
     }
     
@@ -220,34 +234,46 @@ class Model
     // Insertar, recibimos un $_GET o $_POST
     public function create($data)
     {
+        // Convierte el array asociativo en una cadena separado por comas con los nombres de las columnas
+        //Ej si data es ['nombre' => 'Juan', 'apellidos' => 'Pérez'] array keys devuelve ['nombre', 'apellidos']
+        //Y el implode lo convierte en 'nombre, apellidos'
         $columns = implode(', ', array_keys($data));
+        //Crea los mismos huecos con ? que columnas haya en el array $data
         $placeholders = implode(', ', array_fill(0, count($data), '?'));
-
+        //Construye la consulta con la tabla, las columnas de antes y el placeholder con los elementos a sustituir 
         $sql = "INSERT INTO {$this->table} ({$columns}) VALUES ({$placeholders})";
-
+        //Ejecuta la consulta y le pasa los valores del array $data
         $this->query($sql, array_values($data));
 
         return $this;
     }
 
+    //Actualizar registro en la BBDD
     public function update($id, $data)
     {
+        //array_keys pilla el nombre de las columnas de $data
+        //array_map pone el ? en cada columna ej ['nombre = ?', 'apellidos = ?']
+        //implode convierte todo en una cadena tipo: 'nombre = ?, apellidos = ?'
         $fields = implode(', ', array_map(fn($key) => "{$key} = ?", array_keys($data)));
 
+        //Crea la consulta con la tabla, los campos de antes y el placeholder con los elementos a sustituir
         $sql = "UPDATE {$this->table} SET {$fields} WHERE id = ?";
-
+        //Ejecuta la consulta y le pasa los valores del array $data
         $values = array_values($data);
+        //Agrega el id del registro a actualizar que va en el ? del where
         $values[] = $id;
-
+        //Ejecuta la consulta
         $this->query($sql, $values);
 
         return $this;
     }
 
+    //Eliminar registro de la BBDD
     public function delete($id)
     {
+        //Crea la consulta con la tabla y el placeholder con el id
         $sql = "DELETE FROM {$this->table} WHERE id = ?";
-
+        //Ejecuta la consulta y le pasa el id
         $this->query($sql, [$id]);
 
         return $this;
@@ -331,12 +357,16 @@ class Model
      */
     public function ejecutarProcedimiento($nombreProcedimiento, $parametros = [])
     {
+        //Para almacenar los placeholders
         $placeholders = [];
         $valores = [];
 
         foreach ($parametros as $nombre => $parametro) {
+            //Si es IN
             if ($parametro['tipo'] == 'IN') {
+                //Agrega el placeholder con el nombre del procedimiento
                 $placeholders[] = ":{$nombre}";
+                //Agrega el valor del valor a introducir en el IN 
                 $valores[$nombre] = $parametro['valor'];
             } elseif ($parametro['tipo'] == 'OUT' || $parametro['tipo'] == 'INOUT') {
                 $placeholders[] = "@{$nombre}";
